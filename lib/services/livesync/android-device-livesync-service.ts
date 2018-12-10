a9e62caaaa126250fbcb2c7befc421c45115a71b
Kristian Dimitrov <kddimitrov@progress.com>
2018-12-10 20:12:36 +0200

    feat: add initial preformance tracking

--- lib/services/livesync/android-device-livesync-service.ts
+++ lib/services/livesync/android-device-livesync-service.ts
@@ -1,4 +1,5 @@
 import { AndroidDeviceLiveSyncServiceBase } from "./android-device-livesync-service-base";
+import { performanceLog } from "../../common/decorators";
 import * as helpers from "../../common/helpers";
 import { LiveSyncPaths } from "../../common/constants";
 import * as path from "path";
@@ -71,6 +72,7 @@ export class AndroidDeviceLiveSyncService extends AndroidDeviceLiveSyncServiceBa
 		await this.device.applicationManager.restartApplication({ appId: deviceAppData.appIdentifier, projectName });
 	}
 
+	@performanceLog()
 	public async beforeLiveSyncAction(deviceAppData: Mobile.IDeviceAppData): Promise<void> {
 		const deviceRootPath = await this.$devicePathProvider.getDeviceProjectRootPath(deviceAppData.device, {
 			appIdentifier: deviceAppData.appIdentifier,
@@ -107,6 +109,7 @@ export class AndroidDeviceLiveSyncService extends AndroidDeviceLiveSyncServiceBa
 		return true;
 	}
 
+	@performanceLog()
 	public async removeFiles(deviceAppData: Mobile.IDeviceAppData, localToDevicePaths: Mobile.ILocalToDevicePathData[]): Promise<void> {
 		const deviceRootPath = await this.$devicePathProvider.getDeviceProjectRootPath(deviceAppData.device, {
 			appIdentifier: deviceAppData.appIdentifier,
